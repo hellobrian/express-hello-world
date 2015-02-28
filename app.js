f83e0504f4f7525ed4360a21976e7447ae3d30b9
thisisbrianhan <bthan@us.ibm.com>
2015-02-27 22:28:47 -0600

    Set up app.js for dust and output hello world and message

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var cons = require('consolidate');
+var dustjs = require('dustjs-linkedin');
 
 var app = express();
 
 app.set('appName', 'hello-world');
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
+app.engine('dust', cons.dust);
+app.set('view engine', 'dust');
+
+app.all('/', function(req, res) {
+  res.render('index', {msg: 'Welcome to Practical Node.js'});
+});
+
+http.createServer(app).listen(app.get('port'), function() {
+  console.log('Server listening on port ' + app.get('port'));
+});
 
 console.log(app.get('appName'));
